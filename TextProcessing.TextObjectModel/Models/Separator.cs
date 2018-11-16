f240a74fee31961d6c44cc7b6f5046701d3af60b
dlexion <dlexion@yandex.ru>
2018-11-16 23:31:15 +0300

    Add a Separator class

---
+++ TextProcessing.TextObjectModel/Models/Separator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextProcessing.TextObjectModel.Models
+{
+    public class Separator
+    {
+        public string Symbols { get; set; }
+
+        public Separator(string symbols)
+        {
+            Symbols = symbols;
+        }
+
+        public override string ToString()
+        {
+            return Symbols;
+        }
+    }
+}
