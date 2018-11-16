4fd966262023c042ecc2151ce6d64f3243e0531e
dlexion <dlexion@yandex.ru>
2018-11-16 23:26:44 +0300

    Add a Word class

---
+++ TextProcessing.TextObjectModel/Models/Word.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextProcessing.TextObjectModel.Models
+{
+    public class Word
+    {
+        public string Symbols { get; private set; } = string.Empty;
+
+        public Word(string symbols)
+        {
+            Symbols = symbols;
+        }
+
+        public int Length
+        {
+            get => Symbols.Length;
+        }
+
+        public override string ToString()
+        {
+            return Symbols;
+        }
+    }
+}
