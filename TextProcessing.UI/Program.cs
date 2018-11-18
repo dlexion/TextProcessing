f7b9388310f5b5124351693da3115e3069f80708
dlexion <dlexion@yandex.ru>
2018-11-18 20:18:38 +0300

    Add path to configuration manager and parse file

--- TextProcessing.UI/Program.cs
+++ TextProcessing.UI/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextProcessing.Parsing;
+using TextProcessing.TextObjectModel.Models;
 
 namespace TextProcessing.UI
 {
@@ -10,6 +14,37 @@ namespace TextProcessing.UI
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var inputPath = ConfigurationManager.AppSettings["InputPath"];
+
+                var text = new Text();
+                var p = new TextParser();
+
+                using (var sr = new StreamReader(inputPath))
+                {
+                    p.Parse(text, sr);
+                }
+
+                Console.WriteLine("Text:");
+                Console.WriteLine(text);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
