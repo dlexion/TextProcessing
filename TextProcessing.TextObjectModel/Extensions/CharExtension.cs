97ccce01e1b9b3e6dea32bc028bd7968544210d5
dlexion <dlexion@yandex.ru>
2018-11-19 01:11:48 +0300

    Add new method to Word

---
+++ TextProcessing.TextObjectModel/Extensions/CharExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextProcessing.TextObjectModel.Extensions
+{
+    public static class CharExtension
+    {
+        private static readonly char[] vovels = new[] { 'a', 'e', 'i', 'o', 'u' };
+
+        public static bool IsVovel(this char symbol)
+        {
+            return symbol != char.MinValue ? vovels.Contains(char.ToLowerInvariant(symbol)) : false;
+        }
+
+        public static bool IsConsonant(this char symbol)
+        {
+            return !IsVovel(symbol);
+        }
+    }
+}
--- TextProcessing.TextObjectModel/Models/Word.cs
+++ TextProcessing.TextObjectModel/Models/Word.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextProcessing.TextObjectModel.Extensions;
 using TextProcessing.TextObjectModel.Interfaces;
 
 namespace TextProcessing.TextObjectModel.Models
@@ -16,6 +17,11 @@ namespace TextProcessing.TextObjectModel.Models
         public int Length
         {
             get => Symbols.Length;
+        }
+
+        public bool StartWithConsonant()
+        {
+            return Symbols.FirstOrDefault().IsVovel();
         }
     }
 }
